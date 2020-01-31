30f2e74d330cbf2864d095407107cd28fa0678a7
Howard Zhang <how613@hotmail.com>
2020-01-30 20:24:58 -0500

    more stuff

---
+++ Assets/Scripts/gameController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class gameController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    // serves to initialize and store some important values
+    public static int _playerScore;
+    public static int _playerCombo;
+    private Text _pCombo;
+    private Text _pScore;
+    void Start()
+    {
+       _playerScore = 0;
+       _playerCombo = 1; 
+       _pCombo = GameObject.Find("scoreText").GetComponent<Text>();
+       _pScore = GameObject.Find("comboText").GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    private void Update() {
+        _pCombo.text = _playerCombo + "x";
+        _pScore.text = _playerScore + "";
+    }
+}
