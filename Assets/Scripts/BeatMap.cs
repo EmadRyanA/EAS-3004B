6e9ac6e7abf0ec052921bcd4861d822524add781
Evan Jesty <evanjesty@gmail.com>
2020-02-14 16:14:19 -0500

    whoops pt2 - Upload proper BeatMap file

--- Assets/Scripts/BeatMap.cs
+++ Assets/Scripts/BeatMap.cs
@@ -3,14 +3,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[Serializable()]
+/*  Stores the generated beatmap
+ * 
+ */
+ [Serializable()]
 public class BeatMap
 {
 
-    public bga_settings settings;
-    public string name;
-    public BeatMap (bga_settings settings, string name) {
-        this.settings = settings;
+    private List<LaneObject> laneObjectStore; //Store as list since queue cannot be seralized
+    private bga_settings bga_settings; //Settings used to create this Beatmap
+    public string name { get; set; }
+
+    public BeatMap (bga_settings bga_settings, string name)
+    {
+        this.bga_settings = bga_settings;
         this.name = name;
+        laneObjectStore = new List<LaneObject>();
+    }
+
+    public void addLaneObject(LaneObject laneObject)
+    {
+        //todo check if valid... for now this is left to bga to make sure
+        laneObjectStore.Add(laneObject);
+    }
+
+    public Queue<LaneObject> initLaneObjectQueue ()
+    {
+        //todo: load all LaneObject from laneObjectStore into a queue
+        return null;
     }
 }
