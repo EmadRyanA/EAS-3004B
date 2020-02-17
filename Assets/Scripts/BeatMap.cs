f834a95d3a8fb2ef3648334a3a458f849b71f4e0
Evan Jesty <evanjesty@gmail.com>
2020-02-17 17:45:18 -0500

    Revert "Merge branch 'master' of https://github.com/EmadRyanA/EAS-3004B"
    This reverts commit ed54871dafcc3df5f06d11b3824f4e22dc5f7314, reversing
    changes made to e30642e48ab1f436c122d6baba0e6d944bf4fe9b.

--- Assets/Scripts/BeatMap.cs
+++ Assets/Scripts/BeatMap.cs
@@ -3,33 +3,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-/*  Stores the generated beatmap
- * 
- */
- [Serializable()]
+[Serializable()]
 public class BeatMap
 {
 
-    private List<LaneObject> laneObjectStore; //Store as list since queue cannot be seralized
-    private bga_settings bga_settings; //Settings used to create this Beatmap
-    public string name { get; set; }
-
-    public BeatMap (bga_settings bga_settings, string name)
-    {
-        this.bga_settings = bga_settings;
+    public bga_settings settings;
+    public string name;
+    public BeatMap (bga_settings settings, string name) {
+        this.settings = settings;
         this.name = name;
-        laneObjectStore = new List<LaneObject>();
-    }
-
-    public void addLaneObject(LaneObject laneObject)
-    {
-        //todo check if valid... for now this is left to bga to make sure
-        laneObjectStore.Add(laneObject);
-    }
-
-    public Queue<LaneObject> initLaneObjectQueue ()
-    {
-        //todo: load all LaneObject from laneObjectStore into a queue
-        return null;
     }
 }
