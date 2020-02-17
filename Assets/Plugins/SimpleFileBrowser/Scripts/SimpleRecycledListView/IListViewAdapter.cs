95b4471212be7021f1ebbd4de38cbb85ff6dce92
Evan Jesty <evanjesty@gmail.com>
2020-02-17 18:16:24 -0500

    manually remove broken merge artifacts

--- Assets/Plugins/SimpleFileBrowser/Scripts/SimpleRecycledListView/IListViewAdapter.cs
+++ Assets/Plugins/SimpleFileBrowser/Scripts/SimpleRecycledListView/IListViewAdapter.cs
@@ -13,21 +13,4 @@
 
 		void SetItemContent( ListItem item );
 	}
-=======
-﻿namespace SimpleFileBrowser
-{
-	public delegate void OnItemClickedHandler( ListItem item );
-
-	public interface IListViewAdapter
-	{
-		OnItemClickedHandler OnItemClicked { get; set; }
-
-		int Count { get; }
-		float ItemHeight { get; }
-
-		ListItem CreateItem();
-
-		void SetItemContent( ListItem item );
-	}
->>>>>>> 431160aa739fa61569cf147d0576d59f0d0da843
 }
