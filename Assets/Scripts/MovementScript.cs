0b361e89d155de78201f069efe1ba2ab7a2bb8f6
Howard Zhang <how613@hotmail.com>
2020-02-17 15:29:07 -0500

    test

--- Assets/Scripts/MovementScript.cs
+++ Assets/Scripts/MovementScript.cs
@@ -46,10 +46,16 @@ public class MovementScript : MonoBehaviour
         //rb.position = Vector3.Lerp(rb.position, new Vector3(moveHorizontal * shift_amount, rb.position.y, rb.position.z), perc); // orig: 0.25f
         //rb.position = Vector3.Lerp(rb.position, new Vector3(moveHorizontal * shift_amount, rb.position.y, rb.position.z), 0.25f); // orig: 0.25f
         
+<<<<<<< HEAD
         //Debug.Log(rb.position.x);
         float step = shift_speed*Time.deltaTime;
         transform.position = Vector3.MoveTowards(transform.position, new Vector3(moveHorizontal * shift_amount, transform.position.y, transform.position.z), step);
 
+=======
+        float step = shift_speed * Time.deltaTime;
+        rb.position = Vector3.MoveTowards(rb.position, new Vector3(moveHorizontal * shift_amount, rb.position.y, rb.position.z), step);
+        
+>>>>>>> e75878660a947b9178133e70bb2e44adb63d4141
         // this should always be moving in the direction of the plane
         rb.velocity = new Vector3(0, rb.velocity.y, movement_speed);
 
---
+++ Assets/Scripts/TileManager.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class TileManager : MonoBehaviour
+{
+    public GameObject[] tilePrefabs;
+    private List<GameObject> activeTiles;
+
+    private Transform shiftPlayer;
+    private float playerArea = 75.0f;
+    private float zCreated = -15.0f;
+    private float tileLength = 75.0f;
+    public int maxTileCount = 9;
+    private int lastPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        shiftPlayer = GameObject.FindGameObjectWithTag("Player").transform;
+        activeTiles = new List<GameObject>();
+        for(int n = 0; n < maxTileCount; n++)
+        {
+            if (n < 2)
+            {
+                SpawnTile(0);
+            }
+            else
+            {
+                SpawnTile();
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(shiftPlayer.position.z - playerArea > (zCreated - maxTileCount * tileLength))
+        {
+
+            SpawnTile();
+            DeleteTile();
+        }
+    }
+
+    private void SpawnTile(int prefabIndex = -1)
+    {
+        GameObject tileIns;
+        if(prefabIndex == -1)
+        {
+            tileIns = Instantiate(tilePrefabs[RandomPrefab()]) as GameObject;
+        }
+        else
+        {
+            tileIns = Instantiate(tilePrefabs[prefabIndex]) as GameObject;
+        }
+        tileIns.transform.SetParent(transform);
+        tileIns.transform.position = Vector3.forward * zCreated;
+        zCreated += tileLength;
+        activeTiles.Add(tileIns);
+    }
+
+    private void DeleteTile()
+    {
+        Destroy(activeTiles[0]);
+        activeTiles.RemoveAt(0);
+    }
+
+    private int RandomPrefab()
+    {
+        if (tilePrefabs.Length <= 1)
+        {
+            return 0;
+        }
+        int rngIndex = lastPrefab;
+        while(rngIndex == lastPrefab)
+        {
+            rngIndex = Random.Range(0,tilePrefabs.Length);
+        }
+        lastPrefab = rngIndex;
+        return rngIndex;
+    }
+}
