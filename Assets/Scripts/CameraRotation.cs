a877000af9bbc03347b91d07e178a39c831ce8ad
Howard Zhang <how613@hotmail.com>
2020-02-28 12:44:25 -0500

    fix bg rotation

--- Assets/Scripts/CameraRotation.cs
+++ Assets/Scripts/CameraRotation.cs
@@ -10,15 +10,21 @@ public class CameraRotation : MonoBehaviour
     private Vector3 velocity = Vector3.zero;
     public float smoothTime = 20f;
     private Quaternion rotatedValue;
+    private Quaternion oppositeRotatedValue;
     private Vector3 initialRotation;
     public GameObject player;
+    private GameObject bgImage;
     void Start(){
         //Camera cam = gameObject.GetComponent<Camera>();
         
         rotatedValue = Quaternion.Euler(transform.eulerAngles.x, transform.eulerAngles.y, 0.0f);
+        oppositeRotatedValue = Quaternion.Euler(transform.eulerAngles.x, transform.eulerAngles.y, 0.0f);
         
         // stores the initial rotation values
         initialRotation = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y, 0.0f);
+
+        bgImage = transform.GetChild(0).gameObject;
+
         Debug.Log(initialRotation);
     }
 
@@ -33,16 +39,19 @@ public class CameraRotation : MonoBehaviour
             //if(Math.Abs(transform.eulerAngles.z) < rotateIntensity){
             //Debug.Log(moveHorizontal);
             rotatedValue = Quaternion.Euler(initialRotation.x, initialRotation.y, player.transform.eulerAngles.z+rotateIntensity*moveHorizontal);
+            oppositeRotatedValue = Quaternion.Euler(initialRotation.x, initialRotation.y, player.transform.eulerAngles.z-(rotateIntensity/8)*moveHorizontal);
             //}
         }else{
             rotatedValue = Quaternion.Euler(initialRotation.x, initialRotation.y, player.transform.eulerAngles.z);
+            oppositeRotatedValue = rotatedValue;
         }
 
         //transform.eulerAngles = rotatedValue;
         //transform.eulerAngles = Vector3.Lerp(transform.eulerAngles, rotatedValue, 0.25f);
         //Debug.Log(transform.rotation + " " + rotatedValue);
         //transform.rotation = Vector3.SmoothDamp(transform.rotation, rotatedValue, ref velocity, smoothTime);
+        print(transform.rotation);
         transform.rotation = Quaternion.RotateTowards(transform.rotation, rotatedValue, smoothTime*Time.deltaTime);
-        
+        bgImage.transform.rotation = Quaternion.RotateTowards(bgImage.transform.rotation, oppositeRotatedValue, smoothTime*2*Time.deltaTime);
     }
 }
